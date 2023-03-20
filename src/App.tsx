a5fa859721c68c397f6a273d4ad3e6084d4aeac0
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-20 15:05:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Content from './components/content/Content';
+import Home from './views/Home';
+import API from './views/API';
+import NotFound from './views/NotFound';
+
 function App() {
-  return <>Hello CRUD</>;
+  return (
+    <Routes>
+      <Route element={<Content />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/api' element={<API />} />
+        <Route path='/*' element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
