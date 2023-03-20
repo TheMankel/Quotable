9a580cbc902a1172e9e19676b8ef691770995987
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-20 16:43:30 +0100

    Added Heading to Home

--- src/components/content/HomeContent/HomeContent.tsx
+++ src/components/content/HomeContent/HomeContent.tsx
@@ -1,9 +1,11 @@
 import List from './List/List';
 import Form from './Form/Form';
+import Heading from './Heading/Heading';
 
 const HomeContent = () => {
   return (
     <>
+      <Heading title='My List Item' />
       <Form />
       <List />
     </>
