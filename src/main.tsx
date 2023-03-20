594c7909777a44d13c0cc3afa403ba41e2e0c288
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-20 15:05:18 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import ListContextProvider from './store/ListContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ListContextProvider>
+        <App />
+      </ListContextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
