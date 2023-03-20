76c6ae29e876c613052a90d14d3d29842b0db6a3
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-20 16:43:41 +0100

    Added Icons

---
+++ src/components/icons/EditIcon.tsx
@@ -0,0 +1,13 @@
+const EditIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      focusable='false'
+      aria-hidden='true'
+      viewBox='0 0 24 24'>
+      <path d='M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z'></path>
+    </svg>
+  );
+};
+
+export default EditIcon;
