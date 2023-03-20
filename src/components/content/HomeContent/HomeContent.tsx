5f3de76a4e11cddd0c605ec7363233b3e3dbbc45
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-20 15:04:01 +0100

    Created HomeContent

---
+++ src/components/content/HomeContent/HomeContent.tsx
@@ -0,0 +1,13 @@
+import List from './List/List';
+import Form from './Form/Form';
+
+const HomeContent = () => {
+  return (
+    <>
+      <Form />
+      <List />
+    </>
+  );
+};
+
+export default HomeContent;
