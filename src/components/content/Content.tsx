1fd57bd317ef4bd9b4eabf7dd2d2bc40e411505f
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-21 08:46:51 +0100

    Content refactor

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../UI/Footer/Footer';
+import NavBar from '../UI/NavBar/NavBar';
 import styles from './Content.module.css';
 
 const Content = () => {
   return (
-    <main className={styles.root}>
-      <Outlet />
-    </main>
+    <>
+      <NavBar />
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   );
 };
 
