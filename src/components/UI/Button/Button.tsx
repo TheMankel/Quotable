334cfc998b643adbb9d8ef7591cb76101bbf2f76
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-23 20:26:55 +0100

    Created button component

---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,18 @@
+import styles from './Button.module.css';
+
+type Props = {
+  title: string;
+  onClickHandler: () => void;
+};
+
+const Button = ({ title, onClickHandler }: Props) => {
+  return (
+    <div className={styles.wrapper}>
+      <button className={styles.button} type='button' onClick={onClickHandler}>
+        {title}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
