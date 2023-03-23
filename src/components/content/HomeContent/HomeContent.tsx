6d07fb3ada147fb4bfd425b5b5e83ae82141ee9c
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-23 20:36:43 +0100

    Update HomeContent.tsx

--- src/components/content/HomeContent/HomeContent.tsx
+++ src/components/content/HomeContent/HomeContent.tsx
@@ -1,18 +1,32 @@
+import { useState } from 'react';
 import List from './List/List';
 import Form from './Form/Form';
-import Heading from '../../UI/Heading/Heading';
 import Section from '../../UI/Section/Section';
+import Modal from '../../UI/Modal/Modal';
+import { useEdit } from '../../../hooks/useEdit';
+import Button from '../../UI/Button/Button';
 
 const HomeContent = () => {
+  const { isModalOpen, handleCloseModal, openModal, handleEdit, editedId } =
+    useEdit();
+
   return (
     <>
-      <Section id='Form'>
-        <Heading title='My List Item' />
+      {/* <Section id='Form'>
         <Form />
+      </Section> */}
+      <Section id='CreateNewItem'>
+        <Button title='Add New Item' onClickHandler={openModal} />
       </Section>
       <Section id='ListItem'>
-        <List />
+        <List openHandler={openModal} editHandler={handleEdit} />
       </Section>
+      <Modal
+        title='Edit item'
+        isOpen={isModalOpen}
+        closeHandler={handleCloseModal}>
+        <Form id={editedId} closeHandler={handleCloseModal} />
+      </Modal>
     </>
   );
 };
