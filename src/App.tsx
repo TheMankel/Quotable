d2c7e7cddf349d9aa79a42a35e4e42fda4b4a88b
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-24 11:20:39 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Content from './components/content/Content';
 import Home from './views/Home';
-import API from './views/API';
+import API from './views/RandomQuote';
 import NotFound from './views/NotFound';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route element={<Content />}>
         <Route path='/' element={<Home />} />
-        <Route path='/api' element={<API />} />
+        <Route path='/random-quote' element={<API />} />
         <Route path='/*' element={<NotFound />} />
       </Route>
     </Routes>
