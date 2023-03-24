eb5e49eaafe951fb52d8125d97fef1f8018aa386
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-24 11:26:04 +0100

    Fixed id

--- src/types/Item.ts
+++ src/types/Item.ts
@@ -1,10 +1,12 @@
+import { nanoid } from 'nanoid';
+
 class Item {
   id: string;
   text: string;
 
   constructor(todoText: string) {
     this.text = todoText;
-    this.id = new Date().toISOString() + Math.random();
+    this.id = nanoid();
   }
 }
 
