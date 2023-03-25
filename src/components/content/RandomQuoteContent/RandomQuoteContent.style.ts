b27661002c2074517015bade4aefea388219b370
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-25 10:50:55 +0100

    Refactored RandomQuoteContent styling

--- src/components/content/RandomQuoteContent/RandomQuoteContent.style.ts
+++ src/components/content/RandomQuoteContent/RandomQuoteContent.style.ts
@@ -1,7 +1,10 @@
-.text {
+import styled from 'styled-components';
+
+export const Text = styled.p`
   font-size: 1.25rem;
   font-weight: 600;
   overflow: hidden;
   overflow-wrap: break-word;
   text-align: center;
-}
+  padding: 1rem;
+`;
--- src/components/content/RandomQuoteContent/RandomQuoteContent.tsx
+++ src/components/content/RandomQuoteContent/RandomQuoteContent.tsx
@@ -1,8 +1,8 @@
 import Section from '../../UI/Section/Section';
 import Button from '../../UI/Button/Button';
 import { useFetchData } from '../../../hooks/useFetchData';
-import { useEffect, useState, useCallback, useRef } from 'react';
-import styles from './RandomQuoteContent.module.css';
+import { useEffect, useRef } from 'react';
+import { Text } from './RandomQuoteContent.style';
 
 const API = 'https://api.quotable.io/random';
 
@@ -10,22 +10,7 @@ type Quote = { content: string };
 
 const RandomQuoteContent = () => {
   const { data, isLoading, fetchData } = useFetchData(API);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [quote, setQuote] = useState('');
   const firstRender = useRef(false);
-
-  // const fetchQuote = useCallback(async () => {
-  //   setIsLoading(true);
-  //   try {
-  //     const res = await fetch(API);
-  //     const data = await res.json();
-  //     setQuote(data?.content);
-  //     console.count();
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  //   setIsLoading(false);
-  // }, []);
 
   useEffect(() => {
     if (isLoading || firstRender.current) return;
@@ -35,12 +20,10 @@ const RandomQuoteContent = () => {
   }, []);
 
   return (
-    <>
-      <Section id='RandomQuote'>
-        <Button title='Get Random Quote' onClickHandler={fetchData} />
-        <p className={styles.text}>{(data as Quote)?.content}</p>
-      </Section>
-    </>
+    <Section id='RandomQuote'>
+      <Button title='Get Random Quote' onClickHandler={fetchData} />
+      <Text>{(data as Quote)?.content}</Text>
+    </Section>
   );
 };
 
